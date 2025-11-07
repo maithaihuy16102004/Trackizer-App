234ca9d5a8ea6415748012f1aefbe26e3aff6b61
Mai Thai Huy <maithaihuy16102004@gmail.com>
2025-11-08 01:52:39 +0700

    add Subcrition

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trackizer/common/color_extension.dart';
+import 'package:trackizer/common_widget/segment_button.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -9,10 +10,102 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  bool isSubcription = true;
+  List subArr = [
+    {"name": "Spotify", "icon": "assets/img/spotify_logo.png", "price": "5.99"},
+    {
+      "name": "Youtube Premium",
+      "icon": "assets/img/youtube_logo.png",
+      "price": "18.99"
+    },
+    {
+      "name": "Microsoft Onedrive",
+      "icon": "assets/img/onedrive_logo.png",
+      "price": "29.99"
+    },
+    {"name": "Netflix", "icon": "assets/img/netflix_logo.png", "price": "15.00"}
+  ];
+  List bilArr = [
+    {"name": "Spotify", "date": DateTime(2025, 10, 16), "price": "5.99"},
+    {
+      "name": "Youtube Premium",
+      "date": DateTime(2025, 10, 17),
+      "price": "18.99"
+    },
+    {
+      "name": "Microsoft Onedrive",
+      "date": DateTime(2025, 10, 18),
+      "price": "29.99"
+    },
+    {"name": "Netflix", "icon": "assets/img/netflix_logo.png", "price": "15.00"}
+  ];
   @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor: TColor.gray,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: media.width * 1.1,
+              decoration: BoxDecoration(
+                color: TColor.gray70.withOpacity(0.5),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: SegmentButton(
+                      title: "Your subcription",
+                      isActive: isSubcription,
+                      onPressed: () {
+                        setState(() {
+                          isSubcription = !isSubcription;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Expanded(
+                    child: SegmentButton(
+                      title: "Upcoming bills",
+                      isActive: !isSubcription,
+                      onPressed: () {
+                        setState(() {
+                          isSubcription = !isSubcription;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: subArr.length,
+              itemBuilder: (context, index) {
+                var sObj = subArr[index] as Map? ?? {};
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/view/main_tab/main_tab_view.dart
+++ lib/view/main_tab/main_tab_view.dart
@@ -28,69 +28,107 @@ class _MainTabViewState extends State<MainTabView> {
                   padding:
                       const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
                   child: Stack(
-                    alignment: Alignment.center,
+                    alignment: Alignment.bottomCenter,
                     children: [
-                      Image.asset("assets/img/bottom_bar_bg.png"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      Stack(
+                        alignment: Alignment.center,
                         children: [
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                selectTab = 0;
-                                currentTabview = const HomeView();
-                              });
-                            },
-                            icon: Image.asset(
-                              "assets/img/home.png",
-                              width: 50,
-                              height: 20,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                selectTab = 1;
-                                currentTabview = Container();
-                              });
-                            },
-                            icon: Image.asset(
-                              "assets/img/budgets.png",
-                              width: 50,
-                              height: 20,
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 100,
-                            height: 100,
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                selectTab = 2;
-                                currentTabview = Container();
-                              });
-                            },
-                            icon: Image.asset(
-                              "assets/img/calendar.png",
-                              width: 50,
-                              height: 20,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                selectTab = 3;
-                                currentTabview = Container();
-                              });
-                            },
-                            icon: Image.asset(
-                              "assets/img/creditcards.png",
-                              width: 50,
-                              height: 20,
-                            ),
+                          Image.asset("assets/img/bottom_bar_bg.png"),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectTab = 0;
+                                    currentTabview = const HomeView();
+                                  });
+                                },
+                                icon: Image.asset(
+                                  "assets/img/home.png",
+                                  width: 50,
+                                  height: 20,
+                                  color: selectTab == 0
+                                      ? TColor.white
+                                      : TColor.gray,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectTab = 1;
+                                    currentTabview = Container();
+                                  });
+                                },
+                                icon: Image.asset(
+                                  "assets/img/budgets.png",
+                                  width: 50,
+                                  height: 20,
+                                  color: selectTab == 1
+                                      ? TColor.white
+                                      : TColor.gray,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 100,
+                                height: 100,
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectTab = 2;
+                                    currentTabview = Container();
+                                  });
+                                },
+                                icon: Image.asset(
+                                  "assets/img/calendar.png",
+                                  width: 50,
+                                  height: 20,
+                                  color: selectTab == 2
+                                      ? TColor.white
+                                      : TColor.gray,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectTab = 3;
+                                    currentTabview = Container();
+                                  });
+                                },
+                                icon: Image.asset(
+                                  "assets/img/creditcards.png",
+                                  width: 50,
+                                  height: 20,
+                                  color: selectTab == 3
+                                      ? TColor.white
+                                      : TColor.gray,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.all(50),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: TColor.secondary.withOpacity(0.25),
+                                blurRadius: 10,
+                                offset: const Offset(0, 4),
+                              ),
+                            ],
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Image.asset(
+                            "assets/img/center_btn.png",
+                            width: 55,
+                            height: 55,
+                          ),
+                        ),
                       ),
                     ],
                   ),
